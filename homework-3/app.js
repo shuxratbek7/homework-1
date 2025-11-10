ba71fbb14f8bd7ca5127d3296aa035b7ab025c30
Shuxratbek7 <shuxratbeknematjanov@gmail.com>
2025-11-10 23:19:12 +0500

    homework-3

---
+++ homework-3/app.js
@@ -0,0 +1,89 @@
+"use strict"
+
+// function musbatYokiManfiy (son) {
+//     if (son /2) {
+//         alert("toq son")
+//     }else {
+//         alert("juft son")
+//     }
+// }
+// let a = prompt("Son kiritiing")
+// musbatYokiManfiy(a)
+
+
+
+
+//  function musbatYokiManfiy (son,son2,son3,son4) {
+//     if (a <= son) {
+//         alert("qoniqarsiz")
+//     }else if (a < son2){
+//         alert("qoniqarli")
+//     } else if (a <= son3){
+//         alert("yaxshi")
+//     } else  {
+//         alert("a'lo")
+//     }
+// }
+// let a =  Number(prompt("Son kiritiing"))
+// musbatYokiManfiy(69,79,89,200)
+
+
+// function name1(lt1,lt2,lt3){
+//   if (a <= lt1){
+//     alert("bunday sunmaga chegirma yo'q")
+//   } else if(a < lt2){
+//     alert("sizga 5% chegirma beriladi")
+//   } else {
+//     alert("siz uchun 10% chegirma beriladi")
+//   }
+// }
+
+// let a = Number(prompt("chegirma hisoblash"))
+// name1(50,100,400)
+
+
+// function yoshnitekshir() {
+//     let hozirgiYil = parseInt(prompt("xozirgi yilni kiriting:"));
+//     let tugilganYil = parseInt(prompt("tug'ilgan yilingizni kiriting:"));
+
+//     let yosh = hozirgiYil - tugilganYil;
+//     if (yosh >= 18) {
+//         alert("yoshingiz " + yosh + " da Kirish mumkin ");
+//     } else {
+//         alert("yoshingiz " + yosh + " da Kirish mumkin emas ");
+//     }
+// }
+// yoshnitekshir();
+
+
+
+
+// function parol(son1,son2){
+
+// let b = prompt("emailingizni kiriting");
+// let c = prompt("parolingizni kiriting");
+// if (b == son1 && c == son2)  {
+//     alert("siz kirshingiz mumkin")
+// } else {
+//     alert("siz kirishingiz mumkinmas" )
+// }
+// }
+// parol("admin123", "1234567")
+
+
+
+
+function harorat( s1,s2,s3){
+
+if (a >= s1) {
+    alert("judda isiq")
+}
+else if (a < s2  && a >= 1) {
+    alert("yahshi harorat")
+} 
+else if(a < s3) {
+    alert("muzlik")
+}
+}
+let a = prompt("tempraturani kiriting")
+harorat(30,30,1)
